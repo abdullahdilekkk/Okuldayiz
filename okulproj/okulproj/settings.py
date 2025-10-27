c324271f90dc3bf19ee2e593edf97ac9702ddcf6
Abdullah <125198163+abdullahdilekkk@users.noreply.github.com>
2025-10-27 17:15:13 +0300

    açıklama satırı eklendi

--- okulproj/okulproj/settings.py
+++ okulproj/okulproj/settings.py
@@ -92,7 +92,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-AUTH_USER_MODEL = "accounts.User"
+AUTH_USER_MODEL = "accounts.User"   #burası CustomUser modeli için 
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
