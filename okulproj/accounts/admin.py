9a4e137adbe23e76277177f012dcf2fc03434115
Abdullah <125198163+abdullahdilekkk@users.noreply.github.com>
2025-10-27 17:09:50 +0300

    admin.p düzenlendi ve migrate ler yapıldı

--- okulproj/accounts/admin.py
+++ okulproj/accounts/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as MirasUserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(MirasUserAdmin):
+#alt sınıftsn miras aldık adını aynı kullanmak için as dedik
+
+    list_display = ["username", "email","role", "is_active"]
+    #tablo halinde hangi sütunları göstereceğini belirler
+    search_fields = ("email","first_name","last_name","phone_number")
+
+    ordering = ("email",)
+
+    fieldsets = (
+        (None, {"fields": ("email","password")}),
+        ("Kişisel", {"fields": ("first_name","last_name")}),
+        ("Rol & Yetki", {"fields": ("role","is_active","is_staff","is_superuser","groups","user_permissions")}),
+        ("Önemli Tarihler", {"fields": ("last_login","date_joined")}))
+    
+    add_fieldsets = (
+        (None, {"fields": ("email","password1","password2","role","is_staff","is_active")}),
+    )
+    #custom user yaptığımız için zorunlu
--- okulproj/accounts/models.py
+++ okulproj/accounts/models.py
@@ -33,23 +33,22 @@ class User(AbstractUser):
         USER = "user" , "User"
         OWNER = "owner" , "Okul Sahibi"
 
-
     first_name = models.CharField(max_length=256)
     last_name = models.CharField(max_length=256)
-    email = models.TextField(unique=True)   #Tek email = True
+    email = models.EmailField(max_length=255,unique=True)   #Tek email = True
     phone_number = models.CharField(max_length=256)
     role = models.CharField(max_length=155, default=Role.USER, choices=Role.choices)
     #default olarak User atar seçenekler ise sadece Role sınıfındakilerdir 
 
 
-    city = models.ForeignKey(City, on_delete=models.SET_NULL)   #olamzsa kaydım NULL yap beni direkt silme demek
-    district = models.ForeignKey(District, on_delete=models.SET_NULL)
+    city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True, blank=True)   #olamzsa kaydım NULL yap beni direkt silme demek
+    district = models.ForeignKey(District, on_delete=models.SET_NULL, null=True, blank=True)
 
     verified = models.BooleanField(default=False)
     #diğer alanlar AbstructBaseUser dan geliyor 
 
     USERNAME_FIELD = "email"
-
+    REQUIRED_FIELDS = ["username"]
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
