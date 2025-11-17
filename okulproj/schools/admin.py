6d1a1d7983f5dc6d748049ff33a365b5cb592567
Abdullah <125198163+abdullahdilekkk@users.noreply.github.com>
2025-11-17 17:28:56 +0300

    düzenleme

--- okulproj/schools/admin.py
+++ okulproj/schools/admin.py
@@ -1,4 +1,3 @@
-from atexit import register
 from pyexpat import model
 from django.contrib import admin
 from .models import SchoolFeature, Language, Service, School, SchoolPlan, PlanOption, AdditionalFee, SchoolImage
