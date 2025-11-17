6287ecb05d541f230d3b96686757c79a3739b1cb
Abdullah <125198163+abdullahdilekkk@users.noreply.github.com>
2025-11-17 11:05:56 +0300

    dosya değişimi dizin düzenlenmesi sonucunda çıkan hatalar değiştirildi

--- okulproj/accounts/urls.py
+++ okulproj/accounts/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
 from django.contrib.auth import views as DefaultAuth
-import accounts #dosyamız olan 
+from django.views.generic import TemplateView 
 
 app_name = "accounts"
 
 urlpatterns = [
-    path("login/", DefaultAuth.LoginView.as_view(template_name = "accounts/login.html"), name="login"),
-    path("profile/", DefaultAuth.Template_view.as_view(template_name = "accounts/profile.html"), name="profile"),
-    path ("logout/", DefaultAuth.LogoutView.as_view, name=  "logout")
+    path("login/", DefaultAuth.LoginView.as_view(template_name="accounts/login.html"), name="login"),
+    
+    # 2. GÜVENLİKÇİYİ ARADAN ÇIKARDIK, DİREKT ALETİ KULLANDIK
+    path("profile/", TemplateView.as_view(template_name="accounts/profile.html"), name="profile"),
+    
+    # 3. PARANTEZİ UNUTMA (LogoutView bir sınıf olduğu için () gerekir)
+    path("logout/", DefaultAuth.LogoutView.as_view(), name="logout")
 ]
 #Djangonun default auth yapısını kullanıyorum
 
