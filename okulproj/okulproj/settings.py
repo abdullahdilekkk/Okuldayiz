02a574c449932eb0ce6dfb90f21487df2c0634f7
Abdullah <125198163+abdullahdilekkk@users.noreply.github.com>
2025-11-18 16:58:37 +0300

    deneme geminiye kızdım

--- okulproj/okulproj/settings.py
+++ okulproj/okulproj/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',   #DRF İÇİN GEREK
+    'django_filters',
     "accounts",
     "schools",
 ]
--- okulproj/schools/views.py
+++ okulproj/schools/views.py
@@ -2,12 +2,16 @@ from django.shortcuts import render
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 from .serializers import SchoolSerializers, SchoolDetailSerializers
 from .models import School
+from django_filters.rest_framework import DjangoFilterBackend   #filtreleme için settings e de ekledim
 
 # Create your views here.
 
 class SchoolListAPIView(ListAPIView):
     queryset = School.objects.all()
     serializer_class = SchoolSerializers
+
+    filter_backends = [DjangoFilterBackend]
+    filterset_set =["city", "district", "school_type"]
 
 class SchoolDetailAPIView(RetrieveAPIView):
 
