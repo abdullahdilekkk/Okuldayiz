4bfa4fc64c5ed4403ca144a8aa60cfbb917524a9
Abdullah <125198163+abdullahdilekkk@users.noreply.github.com>
2025-12-01 16:01:41 +0300

    lead modeli yazıldı makegigrations ve migrate ler halledildi

--- okulproj/leads/models.py
+++ okulproj/leads/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.forms import CharField, DateTimeField
+from schools.models import School, SchoolPlan, PlanOption
 # Create your models here.
+class Lead(models.Model):
+    name = models.CharField("Başvuru Yapan Adı", max_length=128)
+    surname = models.CharField("Başvuru Yapan Soyadı", max_length=128)
+    phone_number = models.CharField(max_length=128)
+    email = models.EmailField(blank=True)
+    note = models.TextField(blank=True)
+
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    school_plan = models.ForeignKey(SchoolPlan, on_delete=models.CASCADE)
+    plan_option = models.ManyToManyField(PlanOption, blank=True)
+
+    time = models.DateTimeField(auto_now_add=True)
+
+    called = models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} - {self.school.name}"
