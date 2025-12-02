25c29070581bdd55b775a8653b76c24c6f24472e
Abdullah <125198163+abdullahdilekkk@users.noreply.github.com>
2025-12-02 15:02:03 +0300

    düznelem

--- okulproj/accounts/management/commands/import_locations.py
+++ okulproj/accounts/management/commands/import_locations.py
@@ -9,11 +9,7 @@ class Command(BaseCommand):
 
     def turkce_fix(self ,text):
         text = text.replace("I", "ı").replace("İ", "i")
-
         return text.lower().title()
-
-
-
 
 
     def handle(self, *args, **kwargs):
@@ -27,7 +23,6 @@ class Command(BaseCommand):
         with open(file = file_path_district, encoding='utf-8') as file:
             data_district = json.load(file)
 
-
         for item in data_city:
             City.objects.update_or_create(
                 plate_no=item["id"],
@@ -36,11 +31,8 @@ class Command(BaseCommand):
                     "plate_no": item["id"]
                 }
             )
-
-
         for index_district in data_district:
             try:
-
                 object_of_city = City.objects.get(name__iexact = self.turkce_fix(index_district["name"]))
 
                 for dist in index_district["districts"]:
@@ -52,7 +44,6 @@ class Command(BaseCommand):
                             "city":object_of_city
                         }
                     )
-            
             except City.DoesNotExist:
                 self.stdout.write(self.style.ERROR(f"Şehir Bulunamadı (Atlanıyor): {index_district['name']}"))
                 continue
