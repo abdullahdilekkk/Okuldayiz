aa4fcc2887400ed56e168aeced481f7babe34994
Abdullah <125198163+abdullahdilekkk@users.noreply.github.com>
2025-12-09 15:01:51 +0300

    urls ve view düzenlemeleri yapıldı.

--- okulproj/leads/urls.py
+++ okulproj/leads/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import LeadCreateAPIView,LeadListAPIView
+from .views import LeadCreateAPIView, LeadListAPIView, LeadDetailAPIView
 urlpatterns = [
-    path("create/", LeadCreateAPIView.as_view(), name="lead-create"),
-    path("list/", LeadListAPIView.as_view(), name = "lead-list")
+    path("create/", LeadCreateAPIView.as_view(), name = "lead-create"),
+    path("list/", LeadListAPIView.as_view(), name = "lead-list"),
+    path("detail/<int:pk>/", LeadDetailAPIView.as_view(), name = "lead-detail"),
 ]
--- okulproj/leads/views.py
+++ okulproj/leads/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveUpdateDestroyAPIView
+
 from .serializers import LeadSerializer
 from .models import Lead
-from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
-
 # Filtreleme için gerekli kütüphaneler:
 from django_filters.rest_framework import DjangoFilterBackend # Net eşleşme (ID=5, called=False)
 from rest_framework.filters import SearchFilter, OrderingFilter # Arama (name="Ali") ve Sıralama
 
-# Create your views here.
+
+
 class LeadCreateAPIView(CreateAPIView):
     queryset = Lead.objects.all()
     serializer_class = LeadSerializer
@@ -17,14 +17,23 @@ class LeadCreateAPIView(CreateAPIView):
 
 class LeadListAPIView(ListAPIView):
     serializer_class = LeadSerializer
-    permission_classes = (IsAuthenticated, )
+    permission_classes = [IsAuthenticated]
 
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
-    
+
     filterset_fields = ['called', 'school']
     search_fields = ['name', 'surname']      # Nerede kelime aranacak?
     ordering_fields = ['time', 'name']
 
     def get_queryset(self):
         return Lead.objects.filter(school__owner = self.request.user)
+
+
+class LeadDetailAPIView(RetrieveUpdateDestroyAPIView):
+    serializer_class = LeadSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Lead.objects.filter(school__owner = self.request.user)
+    
     
