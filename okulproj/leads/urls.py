887b4adbb852f838c96763298a8a825c455fb01a
Abdullah <125198163+abdullahdilekkk@users.noreply.github.com>
2025-12-22 18:15:34 +0300

    gün sonu

--- okulproj/leads/urls.py
+++ okulproj/leads/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import LeadCreateAPIView, LeadListAPIView, LeadDetailAPIView
+from .views import LeadCreateAPIView, LeadListAPIView, LeadDetailAPIView,LeadDashboardAPIView
 urlpatterns = [
     path("create/", LeadCreateAPIView.as_view(), name = "lead-create"),
     path("list/", LeadListAPIView.as_view(), name = "lead-list"),
     path("detail/<int:pk>/", LeadDetailAPIView.as_view(), name = "lead-detail"),
+    path('dashboard/', LeadDashboardAPIView.as_view(), name='dashboard-leads'),
 ]
+
