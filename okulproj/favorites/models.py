aeccec58592532ad3cf68d0a243134455f37be5c
Abdullah <125198163+abdullahdilekkk@users.noreply.github.com>
2026-01-13 16:09:38 +0300

    favorites/model.py dosyasını yazdım migrateliyorum

--- okulproj/favorites/models.py
+++ okulproj/favorites/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
+from schools.models import School
+from core.models import BaseModel
 
-# Create your models here.
+
+
+class Favorite(BaseModel):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL , on_delete=models.CASCADE, related_name="favorites")
+
+    school = models.ForeignKey(School, on_delete=models.CASCADE, related_name="favorited_by")
+
+    class Meta:
+        verbose_name = "Favorite"
+        unique_together = ["user", "school"]
+
+    def __str__(self):
+        return f"{self.user} favorited {self.school}"
